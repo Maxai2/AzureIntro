1f66bfdb66cf8c6914ea2fabee739a72cb3d37e3
Махмудов Али Ильгар <Mahm_iz81@itstep.edu.az>
2019-03-05 12:01:39 +0400

    Service

---
+++ Service/reciever/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace reciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cstr = "Endpoint=sb://musicplayerserviceprice.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=qTMNt1QDuDZ3mceg4U2svmoHJiXO5wopAw/qSlZj5mY=";
+            var queue = "playerqueueSecond";
+            var client = new QueueClient(cstr, queue);
+            client.RegisterMessageHandler(OnMessage, OnException);
+            Console.ReadLine();
+        }
+
+        private static async Task OnException(ExceptionReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Exception.Message);
+        }
+
+        private static async Task OnMessage(Message msg, CancellationToken ct)
+        {
+            var text = Encoding.Default.GetString(msg.Body);
+            Console.WriteLine(text + " " + msg.PartitionKey);
+        }
+    }
+}
---
+++ Service/sender/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace sender
+{
+    class Program
+    {
+        static void Main (string[] args) {
+            var cstr = "Endpoint=sb://azerbus.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZlufGFq0isZbcr3rwUVLgvRaj4ItQn20fljXtGfKRvs=";
+            var topic = "azertopic";
+            var topicClient = new TopicClient (cstr, topic);
+            var management = new ManagementClient (cstr);
+            var name = Console.ReadLine ();
+            if (!management.SubscriptionExistsAsync (topic, name).Result) {
+                management.CreateSubscriptionAsync (topic, name).Wait ();
+            }
+            var subscriptionClient = new SubscriptionClient (cstr, topic, name);
+            subscriptionClient.RegisterMessageHandler (OnMessage, OnException);
+            while (true) {
+                var text = Console.ReadLine ();
+                if (text == "quit") break;
+                var bytes = Encoding.Default.GetBytes (text);
+                var msg = new Message (bytes) {
+                    Label = name
+                };
+                topicClient.SendAsync (msg).Wait ();
+            }
+        }
+        private static async Task OnException (ExceptionReceivedEventArgs e) {
+            //System.Console.WriteLine(e.Exception.Message);
+        }
+        private static async Task OnMessage (Message msg, CancellationToken ct) {
+            var text = Encoding.Default.GetString (msg.Body);
+            System.Console.WriteLine ($"{msg.Label}: {text}");
+        }
+}
+}
